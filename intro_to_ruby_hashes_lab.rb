739ac8288e69ef1f5e1728425d3c9691a501426c
Rohan Phillips <rohan@solutions-insurance.com>
2019-07-30 18:00:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,12 +16,12 @@ def monopoly_with_second_tier
 	monopoly = {
 		railroads: {
 			pieces: 4,
-			rent_in_dollars: {
-
-			},
 			names: {
 
 			}
+			rent_in_dollars: {
+
+			}			
 		}
 	}
 	#monopoly[:railroads][:pieces] = 4
