813c6b50d41342e6977721d8a5a057db6a27cfd1
Rohan Phillips <rohan@solutions-insurance.com>
2019-07-30 18:05:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,13 +35,13 @@ def monopoly_with_third_tier
 
 				},
 				pennsylvania_railroad:{
-					
+
 				},
 				b_and_o_railroad:{
-					
+
 				},
 				shortline:{
-					
+
 				}
 			},
 			rent_in_dollars: {
